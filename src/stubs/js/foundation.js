4b8bf9781b4a656c431232a2e338f2aea614d4f5
Zack Warren <zachary.warren@me.com>
2018-11-06 14:13:57 -0700

    Update Foundation JS

--- src/stubs/js/foundation.js
+++ src/stubs/js/foundation.js
@@ -1,104 +1,39 @@
-import $ from 'jquery';
+export { Foundation as Core }       from 'foundation-sites/js/foundation.core';
+import * as CoreUtils               from 'foundation-sites/js/foundation.core.utils';
+export { CoreUtils };
+export { Box }                      from 'foundation-sites/js/foundation.util.box'
+export { onImagesLoaded }           from 'foundation-sites/js/foundation.util.imageLoader';
+export { Keyboard }                 from 'foundation-sites/js/foundation.util.keyboard';
+export { MediaQuery }               from 'foundation-sites/js/foundation.util.mediaQuery';
+export { Motion, Move }             from 'foundation-sites/js/foundation.util.motion';
+export { Nest }                     from 'foundation-sites/js/foundation.util.nest';
+export { Timer }                    from 'foundation-sites/js/foundation.util.timer';
+export { Touch }                    from 'foundation-sites/js/foundation.util.touch';
+export { Triggers }                 from 'foundation-sites/js/foundation.util.triggers';
+// export { Abide }                    from 'foundation-sites/js/foundation.abide';
+export { Accordion }                from 'foundation-sites/js/foundation.accordion';
+export { AccordionMenu }            from 'foundation-sites/js/foundation.accordionMenu';
+export { Drilldown }                from 'foundation-sites/js/foundation.drilldown';
+export { Dropdown }                 from 'foundation-sites/js/foundation.dropdown';
+export { DropdownMenu }             from 'foundation-sites/js/foundation.dropdownMenu';
+export { Equalizer }                from 'foundation-sites/js/foundation.equalizer';
+// export { Interchange }              from 'foundation-sites/js/foundation.interchange';
+// export { Magellan }                 from 'foundation-sites/js/foundation.magellan';
+// export { OffCanvas }                from 'foundation-sites/js/foundation.offcanvas';
+// export { Orbit }                    from 'foundation-sites/js/foundation.orbit';
+export { ResponsiveMenu }           from 'foundation-sites/js/foundation.responsiveMenu';
+export { ResponsiveToggle }         from 'foundation-sites/js/foundation.responsiveToggle';
+export { Reveal }                   from 'foundation-sites/js/foundation.reveal';
+// export { Slider }                   from 'foundation-sites/js/foundation.slider';
+// export { SmoothScroll }             from 'foundation-sites/js/foundation.smoothScroll';
+// export { Sticky }                   from 'foundation-sites/js/foundation.sticky';
+export { Tabs }                     from 'foundation-sites/js/foundation.tabs';
+export { Toggler }                  from 'foundation-sites/js/foundation.toggler';
+// export { Tooltip }                  from 'foundation-sites/js/foundation.tooltip';
+export { ResponsiveAccordionTabs }  from 'foundation-sites/js/foundation.responsiveAccordionTabs';
 
-import { Foundation } from 'foundation-sites/js/foundation.core';
-Foundation.addToJquery($);
-
-// Add Foundation Utils to Foundation global namespace for backwards
-// compatibility.
-
-import { rtl, GetYoDigits, transitionend } from 'foundation-sites/js/foundation.util.core';
-Foundation.rtl = rtl;
-Foundation.GetYoDigits = GetYoDigits;
-Foundation.transitionend = transitionend;
-
-import { Box } from 'foundation-sites/js/foundation.util.box'
-import { onImagesLoaded } from 'foundation-sites/js/foundation.util.imageLoader';
-import { Keyboard } from 'foundation-sites/js/foundation.util.keyboard';
-import { MediaQuery } from 'foundation-sites/js/foundation.util.mediaQuery';
-import { Motion, Move } from 'foundation-sites/js/foundation.util.motion';
-import { Nest } from 'foundation-sites/js/foundation.util.nest';
-import { Timer } from 'foundation-sites/js/foundation.util.timer';
-
-Foundation.Box = Box;
-Foundation.onImagesLoaded = onImagesLoaded;
-Foundation.Keyboard = Keyboard;
-Foundation.MediaQuery = MediaQuery;
-Foundation.Motion = Motion;
-Foundation.Move = Move;
-Foundation.Nest = Nest;
-Foundation.Timer = Timer;
-
-// Touch and Triggers previously were almost purely sede effect driven,
-// so no // need to add it to Foundation, just init them.
-
-import { Touch } from 'foundation-sites/js/foundation.util.touch';
-Touch.init($);
-
-import { Triggers } from 'foundation-sites/js/foundation.util.triggers';
-Triggers.init($, Foundation);
-
-import { Abide } from 'foundation-sites/js/foundation.abide';
-Foundation.plugin(Abide, 'Abide');
-
-import { Accordion } from 'foundation-sites/js/foundation.accordion';
-Foundation.plugin(Accordion, 'Accordion');
-
-import { AccordionMenu } from 'foundation-sites/js/foundation.accordionMenu';
-Foundation.plugin(AccordionMenu, 'AccordionMenu');
-
-import { Drilldown } from 'foundation-sites/js/foundation.drilldown';
-Foundation.plugin(Drilldown, 'Drilldown');
-
-import { Dropdown } from 'foundation-sites/js/foundation.dropdown';
-Foundation.plugin(Dropdown, 'Dropdown');
-
-import { DropdownMenu } from 'foundation-sites/js/foundation.dropdownMenu';
-Foundation.plugin(DropdownMenu, 'DropdownMenu');
-
-import { Equalizer } from 'foundation-sites/js/foundation.equalizer';
-Foundation.plugin(Equalizer, 'Equalizer');
-
-// import { Interchange } from 'foundation-sites/js/foundation.interchange';
-// Foundation.plugin(Interchange, 'Interchange');
-
-// import { Magellan } from 'foundation-sites/js/foundation.magellan';
-// Foundation.plugin(Magellan, 'Magellan');
-
-// import { OffCanvas } from 'foundation-sites/js/foundation.offcanvas';
-// Foundation.plugin(OffCanvas, 'OffCanvas');
-
-// import { Orbit } from 'foundation-sites/js/foundation.orbit';
-// Foundation.plugin(Orbit, 'Orbit');
-
-import { ResponsiveMenu } from 'foundation-sites/js/foundation.responsiveMenu';
-Foundation.plugin(ResponsiveMenu, 'ResponsiveMenu');
-
-import { ResponsiveToggle } from 'foundation-sites/js/foundation.responsiveToggle';
-Foundation.plugin(ResponsiveToggle, 'ResponsiveToggle');
-
-import { Reveal } from 'foundation-sites/js/foundation.reveal';
-Foundation.plugin(Reveal, 'Reveal');
-
-// import { Slider } from 'foundation-sites/js/foundation.slider';
-// Foundation.plugin(Slider, 'Slider');
-
-// import { SmoothScroll } from 'foundation-sites/js/foundation.smoothScroll';
-// Foundation.plugin(SmoothScroll, 'SmoothScroll');
-
-// import { Sticky } from 'foundation-sites/js/foundation.sticky';
-// Foundation.plugin(Sticky, 'Sticky');
-
-import { Tabs } from 'foundation-sites/js/foundation.tabs';
-Foundation.plugin(Tabs, 'Tabs');
-
-import { Toggler } from 'foundation-sites/js/foundation.toggler';
-Foundation.plugin(Toggler, 'Toggler');
-
-// import { Tooltip } from 'foundation-sites/js/foundation.tooltip';
-// Foundation.plugin(Tooltip, 'Tooltip');
-
-import { ResponsiveAccordionTabs } from 'foundation-sites/js/foundation.responsiveAccordionTabs';
-Foundation.plugin(ResponsiveAccordionTabs, 'ResponsiveAccordionTabs');
+export { Foundation }               from 'foundation-sites/js/entries/foundation';
+export { default }                  from 'foundation-sites/js/entries/foundation';
 
 // Initialize Foundation components
 $(document).foundation()
